5e6f9a460dc5b1a5f9d84cb951d77873c32bad07
Suyi-Ajayi Boluwatife <bsuyiajayi@gmail.com>
2025-07-22 12:26:28 +0100

    added techniques to improves fault tolerance like when users enters amount as a string

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const { program } = require("commander");
 const path = require("path");
-const { addExpense, getAllExpenses, listAllExpense, viewSummary, updateExpense, deleteExpense, viewSummaryByMonth,setBudgetForMonth, checkBudgetPerMonth, budgetExceeded, } = require('./util/functionFIle.js')
-
+const {
+  addExpense,
+  getAllExpenses,
+  listAllExpense,
+  viewSummary,
+  updateExpense,
+  deleteExpense,
+  viewSummaryByMonth,
+  setBudgetForMonth,
+  checkBudgetPerMonth,
+  budgetExceeded,
+} = require("./util/functionFIle.js");
 
 // Always put space between the flag and the placeholder<>
 program
@@ -19,58 +29,95 @@ program
   .parse();
 
 const options = program.opts(); // to get the options (e.g. --limit)
-let description = options.description 
-let category = options.category
-let amount = options.amount
+let description = options.description;
+let category = options.category;
+let amount = options.amount;
 let itemId = options.id;
 let summaryByMonth = options.summaryByMonth;
 let budgetForMonth = options.setBudgetForMonth;
 const action = program.args[0]; // to get the first argument
 
 // define the path using the fs module so it would be relative to the ,achine
-const expenseDataPath = path.join(__dirname, "data" , "expenseData.json");
+const expenseDataPath = path.join(__dirname, "data", "expenseData.json");
 const budgetPath = path.join(__dirname, "data", "budget.json");
 
 // define data as an empty array on initialization so we know it's empty
-let allExpenseData = []
+let allExpenseData = [];
 allExpenseData = getAllExpenses(expenseDataPath);
 // console.log(budgetForMonth)
 
 // call for budget checker
-checkBudgetPerMonth(budgetPath)
-
+checkBudgetPerMonth(budgetPath);
 
 // we'll use switch statements for the logic of the project
-switch(action){
+try {
+  switch (action) {
     case "add":
-        addExpense(description, category, amount, expenseDataPath, allExpenseData);
-        break;
+      addExpense(
+        description,
+        category,
+        amount,
+        expenseDataPath,
+        allExpenseData
+      );
+      break;
     case "list":
-        listAllExpense(allExpenseData);
-        break;
+      listAllExpense(allExpenseData);
+      break;
     case "summary":
-        viewSummary(allExpenseData);
-        break;
+      viewSummary(allExpenseData);
+      break;
     case "delete":
-        deleteExpense(itemId, allExpenseData, expenseDataPath);
-        break;
+      deleteExpense(itemId, allExpenseData, expenseDataPath);
+      break;
     case "summaryByMonth":
-        viewSummaryByMonth(summaryByMonth, allExpenseData);
-        break;
+      let filteredMonthlySummary = viewSummaryByMonth(
+        summaryByMonth,
+        allExpenseData
+      );
+      // for getting the month for the corresponding month number
+      const monthName = new Date(0, Number(summaryByMonth) - 1).toLocaleString(
+        "en-US",
+        { month: "long" }
+      );
+      console.log(
+        `Total Expenses for ${monthName}: ₦${filteredMonthlySummary}`
+      );
+      break;
     case "update":
-        updateExpense(itemId, allExpenseData, expenseDataPath, description, category, amount);
-        break;
+      updateExpense(
+        itemId,
+        allExpenseData,
+        expenseDataPath,
+        description,
+        category,
+        amount
+      );
+      break;
     case "setBudgetForMonth":
-        setBudgetForMonth(budgetForMonth, budgetPath);
-        break;
+      setBudgetForMonth(budgetForMonth, budgetPath);
+      break;
     default:
-        console.log(`The action ${action} is not avaiable please read the READme file`);
+      console.log(
+        `The action ${action} is not avaiable please read the READme file`
+      );
+  }
+} catch (error) {
+  console.log(error);
 }
 
-
 // call for budget checker
-checkBudgetPerMonth(budgetPath)
+checkBudgetPerMonth(budgetPath);
 
 // for checking if the budget is exceeded per month
-let budgetAmountForMonth = viewSummaryByMonth(7, allExpenseData);
-budgetExceeded(budgetAmountForMonth, budgetPath);
+if (
+  action === "list" ||
+  action === "summary" ||
+  action === "add" ||
+  action === "update"
+) {
+  // get the current month
+  let currentMonth = new Date().getMonth() + 1
+  let budgetAmountForMonth = viewSummaryByMonth(currentMonth, allExpenseData);
+  budgetExceeded(budgetAmountForMonth, budgetPath);
+}
--- util/functionFIle.js
+++ util/functionFIle.js
@@ -1,4 +1,4 @@
-const { readFileSync, writeFileSync, existsSync, write } = require("fs");
+const { readFileSync, writeFileSync, existsSync } = require("fs");
 
 function addExpense(
   description,
@@ -7,25 +7,47 @@ function addExpense(
   expenseDataPath,
   allExpenseData
 ) {
-  const expenseSchema = {
-    id: getId(allExpenseData),
-    description: description,
-    category: category,
-    amount: amount,
-    dateCreated: new Date().toLocaleString(),
-    dateUpdated: new Date().toLocaleString(),
-  };
-  allExpenseData = [...allExpenseData, expenseSchema];
-  writeFileSync(expenseDataPath, JSON.stringify(allExpenseData));
+  try {
+    if (!description || !category || !amount) {
+      throw new Error(
+        "Description, category, and amount are required to add an expense."
+      );
+    }
+    if (typeof amount !== "number" && isNaN(amount)) {
+      throw new Error("Amount must be a number.");
+    }
+    const expenseSchema = {
+      id: getId(allExpenseData),
+      description: description,
+      category: category,
+      amount: amount,
+      dateCreated: new Date().toLocaleString(),
+      dateUpdated: new Date().toLocaleString(),
+    };
+    allExpenseData = [...allExpenseData, expenseSchema];
+    writeFileSync(expenseDataPath, JSON.stringify(allExpenseData));
+  } catch (error) {
+    console.error(
+      "Error adding expense:",
+      error.message ? error.message : error
+    );
+  }
 }
 
 function getAllExpenses(expenseDataPath) {
-  const file = readFileSync(expenseDataPath, "utf-8");
-  if (file.length === 0) {
-    return [];
-  } else {
-    let allTasks = JSON.parse(file);
-    return allTasks;
+  try {
+    const file = readFileSync(expenseDataPath, "utf-8");
+    if (file.length === 0) {
+      return [];
+    } else {
+      let allTasks = JSON.parse(file);
+      return allTasks;
+    }
+  } catch (error) {
+    console.error(
+      "Error getting expenses:",
+      error.message ? error.message : error
+    );
   }
 }
 
@@ -97,10 +119,13 @@ function viewSummary(allExpenseData) {
 }
 
 function deleteExpense(itemId, allExpenseData, expenseDataPath) {
+  if(!itemId){
+    throw new Error("Item ID is required to delete an expense.");
+  }
   try {
     const newExpenseData = allExpenseData.filter((data) => {
       return data.id !== Number(itemId);
-    })    
+    });
     writeFileSync(expenseDataPath, JSON.stringify(newExpenseData));
     console.log(`Expense with ID ${itemId} has been deleted.`);
   } catch (error) {
@@ -110,63 +135,85 @@ function deleteExpense(itemId, allExpenseData, expenseDataPath) {
 
 function viewSummaryByMonth(summaryByMonth, allExpenseData) {
   try {
+    if (!summaryByMonth) {
+      throw new Error("Month number is required to view summary by month.");  
+    }
     // use chaining as we want to perform double array manipulation technique
     const filteredMonthlySummary = allExpenseData
-    .filter((data) => {
-      // because .getMonth() starts from 0 we'll minus it by one before comparing
-      return (
-        new Date(data.dateCreated).getMonth() === Number(summaryByMonth) - 1
-      );
-    })
-    .reduce(
-      (monthlySum, monthlyData) => {
-        monthlySum += Number(monthlyData.amount)
+      .filter((data) => {
+        // because .getMonth() starts from 0 we'll minus it by one before comparing
+        return (
+          new Date(data.dateCreated).getMonth() === Number(summaryByMonth) - 1
+        );
+      })
+      .reduce((monthlySum, monthlyData) => {
+        monthlySum += Number(monthlyData.amount);
         return monthlySum;
-      },
-      0
-    ).toFixed(2);
+      }, 0)
+      .toFixed(2);
 
-    // for getting the month for the corresponding month number
-    const monthName = new Date(0, Number(summaryByMonth)-1).toLocaleString("en-US", { month: "long" });
-    console.log(`Total Expenses for ${monthName}: ₦${filteredMonthlySummary}`);
     return filteredMonthlySummary;
   } catch (error) {
     console.log("Error viewing summary by month:", error);
   }
 }
 
-function updateExpense(itemId, allExpenseData, expenseDataPath, description, category, amount) {
+function updateExpense(
+  itemId,
+  allExpenseData,
+  expenseDataPath,
+  description,
+  category,
+  amount
+) {
   try {
-    if(!description && !category && !amount) {
-      throw new Error("At least one field (description, category, amount) must be provided for update.");
+    // checks if itemId is provided as it is essential for updating
+    if(!itemId) {
+      throw new Error("Item ID is required to update an expense.");
     }
-    const newAllExpenseData = allExpenseData.map( data => {
-      if (data.id === Number(itemId)){
+    // checks if at least one field is provided for update
+    if (!description && !category && !amount) {
+      throw new Error(
+        "At least one field (description, category, amount) must be provided for update."
+      );
+    }
+    // check if the itemId is is a number
+    if (typeof amount !== "number" && isNaN(amount)) {
+      throw new Error("Amount must be a number.");
+    }
+    const newAllExpenseData = allExpenseData.map((data) => {
+      if (data.id === Number(itemId)) {
         return {
           ...data,
-          description: description? description : data.description,
-          category: category? category : data.category,
-          amount: amount? amount : data.amount,
+          description: description ? description : data.description,
+          category: category ? category : data.category,
+          amount: amount ? amount : data.amount,
           dateUpdated: new Date().toLocaleString(),
-        }
+        };
       }
       return data;
-    })
+    });
     writeFileSync(expenseDataPath, JSON.stringify(newAllExpenseData));
     console.log(`Expense with ID ${itemId} has been updated.`);
   } catch (error) {
-    console.error("Error updating expense:", error.message? error.message : error);
+    console.error(
+      "Error updating expense:",
+      error.message ? error.message : error
+    );
   }
 }
 
-
 function setBudgetForMonth(budgetForMonth, budgetPath) {
   try {
+    // checks if budget is provided
+    if (!budgetForMonth) {
+      throw new Error("Budget for the month must be provided.");
+    }
     const budgetData = {
       budget: budgetForMonth,
-      dateSet: new Date().toLocaleString()
-    }
-    if(!budgetForMonth) {
+      dateSet: new Date().toLocaleString(),
+    };
+    if (!budgetForMonth) {
       throw new Error("Budget for the month must be provided.");
     }
     writeFileSync(budgetPath, JSON.stringify(budgetData));
@@ -179,19 +226,22 @@ function setBudgetForMonth(budgetForMonth, budgetPath) {
 }
 
 // checks if budget per month is currently for this month if not it would request User to change budget as previous one has expired
-function checkBudgetPerMonth(budgetPath){
+function checkBudgetPerMonth(budgetPath) {
   try {
     let monthBudget = readFileSync(budgetPath, "utf-8");
-    monthBudget = JSON.parse(monthBudget)
+    monthBudget = JSON.parse(monthBudget);
     const monthBudgetDate = new Date(monthBudget.dateSet);
     const currentDate = new Date();
-    if(monthBudgetDate.getMonth() !== currentDate.getMonth()){
+    if (monthBudgetDate.getMonth() !== currentDate.getMonth()) {
       // if the month is not the same as the current month then we would write a new budget
-      writeFileSync(budgetPath, JSON.stringify({ budget: "0", dateSet: new Date().toLocaleString() }));
-      throw new Error ("Set new Monthly budget as Previous one has expired")
+      writeFileSync(
+        budgetPath,
+        JSON.stringify({ budget: "0", dateSet: new Date().toLocaleString() })
+      );
+      throw new Error("Set new Monthly budget as Previous one has expired");
     }
   } catch (error) {
-    console.log("Error:", error.message? error.message : error);
+    console.log("Error:", error.message ? error.message : error);
   }
 }
 
@@ -206,17 +256,13 @@ function budgetExceeded(budgetAmount, budgetPath) {
       console.log(
         `Budget exceeded! You have spent ₦${budgetAmount} which is more than your budget of ₦${budget}.`
       );
-    }else if( budgetAmount < budget && budgetAmount > budget - budget * 0.1){
+    } else if (budgetAmount < budget && budgetAmount > budget - budget * 0.1) {
       console.log(
-        `You are close to your budget of ₦${budget}.\nSpend Wisely!`
+        `You are close to your budget of ₦${budget}.\nYou've spent ${budgetAmount} Spend Wisely!`
       );
+    } else {
+      return;
     }
-     else {
-      console.log(
-        `You are within your budget. You have spent ₦${budgetAmount} out of ₦${budget}.`
-      );
-    }
-
   } catch (error) {
     console.error(
       "Error checking if budget is exceeded:",
@@ -247,5 +293,5 @@ module.exports = {
   viewSummaryByMonth,
   setBudgetForMonth,
   budgetExceeded,
-  checkBudgetPerMonth
+  checkBudgetPerMonth,
 };
